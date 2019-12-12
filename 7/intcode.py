5d89b7148a9cea66a172fa8d81e202632e67087d
Dale Halford-Pearce <dale@Dales-MBP.lan>
2019-12-12 22:47:41 +0000

    Added day 7 part 1

--- 7/intcode.py
+++ 7/intcode.py
@@ -1,6 +1,7 @@
 from copy import copy
 from dataclasses import dataclass
 from enum import Enum, auto
+from itertools import permutations
 from typing import List, Optional, Tuple
 
 
@@ -45,6 +46,10 @@ class Instruction:
     parameters: List[Mode]
 
 
+class IntcodeTerminated(Exception):
+    pass
+
+
 def get_instruction(instruction_number: int) -> Instruction:
     code = str(instruction_number)
     op = Operation(int(code[-2:]))
@@ -58,13 +63,13 @@ def get_instruction(instruction_number: int) -> Instruction:
 def get_value(code: List[int], mode: Mode, value: int) -> int:
     if mode == Mode.POSITION:
         return code[value]
-    elif mode == Mocpde.IMMEDIATE:
+    elif mode == Mode.IMMEDIATE:
         return value
     else:
         raise ValueError(f"Unknown mode type {mode}")
 
 
-def intcode(code: List[int]) -> List[int]:
+def intcode(code: List[int], inputs: List[int]) -> int:
     new_code = copy(code)
 
     i = 0
@@ -83,10 +88,10 @@ def intcode(code: List[int]) -> List[int]:
             new_code[result] = lh_value * rh_value
         elif instruction.operation == Operation.INPUT:
             pos = new_code[i + 1]
-            new_code[pos] = int(input("Please input..."))
+            new_code[pos] = inputs.pop(0)
         elif instruction.operation == Operation.OUTPUT:
             pos = new_code[i + 1]
-            print(new_code[pos])
+            return new_code[pos]
         elif instruction.operation in JUMP_OPS:
             check, pointer = new_code[i + 1 : i + 3]
             check_value = get_value(new_code, instruction.parameters[0], check)
@@ -113,22 +118,18 @@ def intcode(code: List[int]) -> List[int]:
 
         instruction = get_instruction(new_code[i])
 
-    return new_code
-
-
-def find_inputs(code: List[int], target: int) -> Tuple[Optional[int], Optional[int]]:
-    for noun in range(100):
-        for verb in range(100):
-            code[1:3] = noun, verb
-            output = intcode(code)
-            if output[0] == target:
-                return noun, verb
-
-    return None, None
+    raise IntcodeTerminated("Program did not output")
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         for line in f:
             code = [int(i) for i in line.split(",")]
-    intcode(code)
+    
+    highest_signal = 0
+    for perm in permutations(range(5)):
+        signal = 0
+        for phase in perm:
+            signal = intcode(code, [phase, signal])
+        highest_signal = max(signal, highest_signal)
+    print(highest_signal)
