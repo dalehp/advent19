8e86b94348dc67f1de5bf299536daa531d66f639
Dale Halford-Pearce <dale@Dales-MBP.lan>
2019-12-15 23:39:43 +0000

    WIP: Day 10 part 2, incomplete

--- 10/asteroid.py
+++ 10/asteroid.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from math import atan2, sqrt
+from math import atan2, sqrt, pi
 from typing import DefaultDict, List, Set, Tuple
 
 
@@ -13,15 +13,20 @@ class Coordinate:
         self.x = x
         self.y = y
         self.r = sqrt(x*x + y*y)
-        self.theta = atan2(y, x)
+        self.theta = - atan2(y, x) - pi / 2
+        if self.theta < 0.0:
+            self.theta += 2 * pi
 
     def difference(self, other) -> Coordinate:
         return Coordinate(other.x - self.x, other.y - self.y)
 
+    def __repr__(self):
+        return f"Coordinate(x={self.x}, y={self.y})"
+
 
 if __name__ == "__main__":
     asteroids: Set[Coordinate] = set()
-    with open("input.txt") as f:
+    with open("test_input.txt") as f:
         for y, line in enumerate(f):
             for x, ast in enumerate(line):
                 if ast == "#":
@@ -29,10 +34,30 @@ if __name__ == "__main__":
 
     highest_count = 0
     for asteroid in asteroids:
-        angles: DefaultDict[float, Coordinate] = defaultdict(set)
+        angles: DefaultDict[float, List[Coordinate]] = defaultdict(list)
         for other in (asteroids - {asteroid}):
             diff = asteroid.difference(other)
-            angles[diff.theta].add(other)
-        highest_count = max(highest_count, len(angles))
+            angles[diff.theta].append(diff)
+
+        if len(angles) > highest_count:
+            highest_count = len(angles)
+            best_angles = angles
+            best_coord = asteroid
     print(highest_count)
-        
+    print(best_coord)
+
+
+    for angle, coords in best_angles.items():
+        coords.sort(key=lambda x: x.r)
+    print()
+    print(sorted(best_angles.keys()))
+    print(best_angles[0.0])
+
+    destruction_order = []
+    while any(coordinates for coordinates in best_angles.values()):
+        for angle in sorted(best_angles.keys()):
+            if best_angles[angle]:
+                destruction_order.append(best_angles[angle].pop(0))
+    winner = destruction_order[199]
+    print([Coordinate(c.x + best_coord.x, c.y + best_coord.y) for c in destruction_order])
+    print(winner.x * 100 + winner.y)
